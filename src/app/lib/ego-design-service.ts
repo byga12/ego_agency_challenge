1b31d45142cf8f0e14918173737bc47f65f88960
Gaby Shima <byga>
2026-01-10 19:44:58 -0300

    Añado validación de status de petición http, corrijo los tipos de return

--- src/app/lib/ego-design-service.ts
+++ src/app/lib/ego-design-service.ts
@@ -1,16 +1,22 @@
+import { CarDetail, CarMin } from "../types/cars";
+
 const API_URL = process.env.API_URL
 
-export const getCars = async (): Promise<string> => {
+export const getCars = async (): Promise<CarMin[]> => {
     const res = await fetch(`${API_URL}/models`)
+    if (!res.ok) {
+        throw new Error(`Error al obtener modelos: ${res.statusText}`);
+    }
     const body = await res.json()
-    console.log(body);
-    
-    return ""
+    return body;
 }
 
-export const getCarById = async (id: number): Promise<string> => {
+export const getCarById = async (id: number): Promise<CarDetail> => {
     const res = await fetch(`${API_URL}/models/${id}`)
+    if (!res.ok) {
+        if (res.status === 404) throw new Error("Auto no encontrado");
+        throw new Error("Error al obtener el detalle del auto");
+    }
     const body = await res.json()
-    console.log(body);
-    return ""
+    return body
 }
